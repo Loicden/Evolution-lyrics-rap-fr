372f99aa8756253e514bb8bc2e71098b4d3692df
maxime-ecl <maxime.manassero@ecl18.ec-lyon.fr>
2021-02-11 15:13:09 +0100

    extraction album et update de fusion par annee

--- fusion_par_annee.py
+++ fusion_par_annee.py
@@ -6,6 +6,7 @@ Created on Thu Feb  4 16:42:14 2021
 """
 import os
 import csv
+from collections import OrderedDict
 
 Path = os.getcwd()
 
@@ -13,7 +14,7 @@ annees = [1980+i for i in range(41)]
 
 def fusion_annee(ANNEE): 
 
-        fichiers = [f for f in os.listdir(Path) if f.endswith(str(ANNEE)+".csv")]
+        fichiers = [f for f in os.listdir(Path) if f.endswith(str(ANNEE)+".csv") and not f.startswith("albums")]
         if len(fichiers) == 0:
             return 
         
@@ -25,14 +26,16 @@ def fusion_annee(ANNEE):
                 linereader = csv.reader(csvfile, delimiter=',', quotechar='|')
                 for row in linereader:
                     word = row[0]
-                    count = int(row[1])
+                    count = float(row[1])
                     if word not in dic.keys():
                         dic.update({word : count})
                     else :
                         dic[word] += count
+        dic = OrderedDict(sorted(dic.items(), key=lambda t: t[0]))
+        sortedDict = OrderedDict(sorted(dic.items(), key=lambda t: t[1], reverse=True))
                     
         with open(Path+"\\albums_"+str(ANNEE)+".csv",  'w', newline='', encoding='utf-8') as csvfile:
-            for key, value in dic.items():
+            for key, value in sortedDict.items():
                     writer = csv.writer(csvfile, delimiter=',')
                     writer.writerow([key, value])
 
